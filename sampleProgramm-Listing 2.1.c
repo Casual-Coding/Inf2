fbb4ec790ba86c19d08c61bd74f25ea83c1296e7
Casual-Coding <marcus_schons@icloud.com>
2023-11-24 20:50:49 +0100

    Initial commit

---
+++ sampleProgramm-Listing 2.1.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+int main()
+    {
+    //instructions
+    int16_t a;
+    int16_t b;
+    int16_t c;
+
+    //sample calculations
+    a = 3;
+    b = 4;
+    c = a + b;
+
+    //console output
+    printf("%d + %d = %d\n", a, b, c);
+    }
---
+++ sampleProgramm-Listing 2.2.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+int main()
+    {
+    //instructions
+    uint8_t a,b,c;
+
+    //sample calculations
+    b = 3;
+    c = 5;
+    a = b + c;
+
+    //console output
+    printf("%d + %d = %d\n", b, c, a);
+    }
