243b90fe5cdd8cd14ccc477007d86f1e010f2a9f
Casual-Coding <marcus_schons@icloud.com>
2023-11-24 21:16:45 +0100

    Update task 2.1.6 converting Celsius to Fahrenheit.c
    implemented a basic check if input is a letter

--- task 2.1.6 converting Celsius to Fahrenheit.c
+++ task 2.1.6 converting Celsius to Fahrenheit.c
@@ -1,20 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <ctype.h>
 
 int main()
     {
     //vars
+    char input[15];
     float grad = 0;
     float fahrenheit;
 
     //input
     printf("bitte geben Sie eine Temperatur in Grad ein, welche Sie in Fahrenheit umrechnen wollen:\n");
-    scanf("%f", &grad);
+    scanf("%s", &input);
 
-    //calculation
-    fahrenheit = (grad * 1.8) + 32;
+    if (sscanf(input, "%f", &grad) ==1)
+            {
+            //calculation
+            fahrenheit = (grad * 1.8) + 32;
 
-    //console output
-    printf("%.1fC entspricht %.1f fahrenheit\n", grad, fahrenheit);
+            //console output
+            printf("%.1fC entspricht %.1f fahrenheit\n", grad, fahrenheit);
+    } else {
+            printf("keine gueltige Zahl eingegeben !");
+            }
     }
