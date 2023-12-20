e639ab5ad3d326d6fb722c2b7212beaff7d99086
MarcusSchons <marcus_schons@icloud.com>
2023-12-20 22:53:48 +0100

    Update task 2.2.4 tool for characterisation of a given Symbol.c

---
+++ task 2.2.4 tool for characterisation of a given Symbol.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    // this tool is supposed to identify a given Symbol
+    int toCheck;
+    // input
+    printf("please input a Single Number, letter or Symbol, this Programm is gonna identify it:\n");
+    scanf("%c", &toCheck);
+    //switch case
+    if (toCheck >= 65 && toCheck <= 90){
+        printf("its a Capital letter");
+    } else if (toCheck >= 97 && toCheck <= 122) {
+        printf("its a small Letter");
+    }  else if (toCheck >= 48 && toCheck <= 57) {
+        printf("its a Number");
+    }  else if (toCheck >= 0 && toCheck <= 31 || toCheck == 127) {
+        printf("Control Symbol");
+    }   else {
+        printf("its a Symbol");
+    }
+}
