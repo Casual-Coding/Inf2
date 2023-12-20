4875cc76d3a86e509b90af1623665934597cc830
MarcusSchons <marcus_schons@icloud.com>
2023-12-20 21:49:11 +0100

    Update task 2.1.11 calculate Area of a Triangle.c
    Programm fully working

--- task 2.1.11 calculate Area of a Triangle.c
+++ task 2.1.11 calculate Area of a Triangle.c
@@ -6,6 +6,7 @@ int main(){
     //this program can calculate the Area of a given triangle using the Gauﬂsche Trapezformel
     // vars
     uint16_t x1, y1, x2, y2, x3, y3;
+    float area, subarea1, subarea2, subarea3;
     // Rectangle Coordinates
     printf("erster Punkt des Dreiecks:\n");
     getXYCoordinates(&x1, &y1);
@@ -13,14 +14,21 @@ int main(){
     getXYCoordinates(&x2, &y2);
     printf("dritter Punkt des Dreiecks:\n");
     getXYCoordinates(&x3, &y3);
-    // Calculations
-
+    // Calculations using  2A=Summe xi * (yi+1-yi-1)
+    subarea1 = x1 * (y2 - y3);
+    subarea2 = x2 * (y3 - y1);
+    subarea3 = x3 * (y1 - y2);
+    area = (subarea1 + subarea2 + subarea3) / 2;
+    if (area < 0) {
+        area = area * -1;
+    }
     // output
+    printf("Der Fl‰cheninhalt des Dreiecks betr‰gt %.1f FE", area);
 }
 
 void getXYCoordinates(uint16_t *x, uint16_t *y){
     printf("bitte geben Sie die X Koordinate ein:\n");
-    scanf("%i", x);
+    scanf("%hu", x);
     printf("bitte geben Sie die Y Koordinate ein:\n");
-    scanf("%i", y);
+    scanf("%hu", y);
 }
