c56672c324bdf7f15d6d3400d6ea187f57988004
MarcusSchons <marcus_schons@icloud.com>
2024-01-29 12:12:02 +0100

    Update c++ ui example.cpp

--- c++ ui example.cpp
+++ c++ ui example.cpp
@@ -2,20 +2,92 @@
 
 HWND button, textLabel;
 
+typedef struct tagLOGFONTW {
+  LONG      lfHeight;
+  LONG      lfWidth;
+  LONG      lfEscapement;
+  LONG      lfOrientation;
+  LONG      lfWeight;
+  BYTE      lfItalic;
+  BYTE      lfUnderline;
+  BYTE      lfStrikeOut;
+  BYTE      lfCharSet;
+  BYTE      lfOutPrecision;
+  BYTE      lfClipPrecision;
+  BYTE      lfQuality;
+  BYTE      lfPitchAndFamily;
+  WCHAR     lfFaceName[LF_FACESIZE];
+} LOGFONTW;
+
+typedef struct tagTEXTMETRICW {
+  LONG tmHeight;
+  LONG tmAscent;
+  LONG tmDescent;
+  LONG tmInternalLeading;
+  LONG tmExternalLeading;
+  LONG tmAveCharWidth;
+  LONG tmMaxCharWidth;
+  LONG tmWeight;
+  LONG tmOverhang;
+  LONG tmDigitizedAspectX;
+  LONG tmDigitizedAspectY;
+  WCHAR tmFirstChar;
+  WCHAR tmLastChar;
+  WCHAR tmDefaultChar;
+  WCHAR tmBreakChar;
+  BYTE tmItalic;
+  BYTE tmUnderlined;
+  BYTE tmStruckOut;
+  BYTE tmPitchAndFamily;
+  BYTE tmCharSet;
+} TEXTMETRICW;
+
+int pointY = 50; // Ausgangsposition des Punktes
+
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     switch (uMsg) {
         case WM_CREATE: {
-            button = CreateWindowW(L"BUTTON", L"Click me", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
-                                   10, 10, 100, 30, hwnd, (HMENU)1, (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE), NULL);
-
-            textLabel = CreateWindowW(L"STATIC", L"Hello, world!", WS_VISIBLE | WS_CHILD | SS_LEFT,
-                                      10, 50, 200, 20, hwnd, (HMENU)2, (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE), NULL);
+            CreateWindowW(L"BUTTON", L"Click me", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+                          10, 10, 100, 30, hwnd, (HMENU)1, (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE), NULL);
             break;
         }
         case WM_COMMAND: {
             if (LOWORD(wParam) == 1) {
-                SetWindowTextW(textLabel, L"Button clicked!");
+                // Lade dynamisch die GDI-Funktionen
+                HMODULE hGDI = LoadLibraryW(L"gdi32.dll");
+                if (hGDI != NULL) {
+                    typedef HBRUSH(WINAPI *CREATEBRUSHFUNC)(COLORREF);
+                    CREATEBRUSHFUNC CreateSolidBrushFunc = (CREATEBRUSHFUNC)GetProcAddress(hGDI, "CreateSolidBrush");
+                    if (CreateSolidBrushFunc != NULL) {
+                        HBRUSH redBrush = CreateSolidBrushFunc(RGB(255, 0, 0));
+
+                        // Zeichne den Punkt im Fenster
+                        HDC hdc = GetDC(hwnd);
+                        SelectObject(hdc, redBrush);
+                        Ellipse(hdc, 400 - 2, pointY - 2, 400 + 2, pointY + 2); // Mittelpunkt (400, pointY)
+                        ReleaseDC(hwnd, hdc);
+
+                        // Bewege den Punkt nach unten für die nächste Position
+                        pointY += 5;
+
+                        // Freigebe der Ressourcen
+                        DeleteObject(redBrush);
+                    }
+
+                    // Freigabe der Bibliothek
+                    FreeLibrary(hGDI);
+                }
             }
+            break;
+        }
+        case WM_PAINT: {
+            // Diese Zeichenprozedur wird aufgerufen, wenn das Fenster neu gezeichnet werden muss
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+
+            // Hier könntest du weitere Zeichenoperationen durchführen, wenn nötig
+
+            EndPaint(hwnd, &ps);
             break;
         }
         case WM_DESTROY: {
